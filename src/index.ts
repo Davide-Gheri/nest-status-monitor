45236ff3251ffe1a03024cbe6053f2e5c1f0e785
Davide Gheri <davide.gheri@docomodigital.com>
2019-02-27 12:49:17 +0100

    updated index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { StatusMonitorModule } from './status.monitor.module';
-import { StatusMonitorConfiguration } from './config/status.monitor.configuration';
-import { ChartVisibilityConfiguration } from './config/chart.visibility.configuration';
-import { HealthCheckConfiguration } from './config/health.check.configuration';
-import { SpansConfiguration } from './config/spans.configuration';
 
-export {
-  StatusMonitorModule,
-  StatusMonitorConfiguration,
-  ChartVisibilityConfiguration,
-  HealthCheckConfiguration,
-  SpansConfiguration,
-};
+export * from './status.monitor.module';
+export * from './interfaces/status-monitor-module-options.interface';
+export * from './interfaces/health-check-options.interface';
+export * from './interfaces/chart-visibility-options.interface';
+export * from './interfaces/spans-options.interface';
